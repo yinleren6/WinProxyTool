3d605fe6092d23ec064d4399e160e368fbea495f
yinleren6 <52646375+yinleren6@users.noreply.github.com>
2022-05-25 19:59:50 +0800

    updataCreate README.md

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,252 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_config = new System.Windows.Forms.Button();
+            this.label_Port = new System.Windows.Forms.Label();
+            this.textBox_Port = new System.Windows.Forms.TextBox();
+            this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.label_IP = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.CheckBox_Override = new System.Windows.Forms.CheckBox();
+            this.log = new System.Windows.Forms.RichTextBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label_versiom = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button_refresh);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button_config);
+            this.panel1.Location = new System.Drawing.Point(23, 257);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(384, 41);
+            this.panel1.TabIndex = 2;
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.Font = new System.Drawing.Font("宋体", 14F);
+            this.button_refresh.Location = new System.Drawing.Point(3, 3);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(120, 35);
+            this.button_refresh.TabIndex = 5;
+            this.button_refresh.Text = "刷新";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.Button_get_Proxy_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 14F);
+            this.button1.Location = new System.Drawing.Point(132, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 35);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "switch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_Enable_Proxy_Click);
+            // 
+            // button_config
+            // 
+            this.button_config.Font = new System.Drawing.Font("宋体", 14F);
+            this.button_config.Location = new System.Drawing.Point(261, 3);
+            this.button_config.Name = "button_config";
+            this.button_config.Size = new System.Drawing.Size(120, 35);
+            this.button_config.TabIndex = 6;
+            this.button_config.Text = "保存";
+            this.button_config.UseVisualStyleBackColor = true;
+            this.button_config.Click += new System.EventHandler(this.Button_Save_Proxy_Click);
+            // 
+            // label_Port
+            // 
+            this.label_Port.AutoSize = true;
+            this.label_Port.Font = new System.Drawing.Font("宋体", 14F);
+            this.label_Port.Location = new System.Drawing.Point(3, 0);
+            this.label_Port.Name = "label_Port";
+            this.label_Port.Size = new System.Drawing.Size(50, 26);
+            this.label_Port.TabIndex = 4;
+            this.label_Port.Text = "端口";
+            // 
+            // textBox_Port
+            // 
+            this.textBox_Port.Font = new System.Drawing.Font("宋体", 14F);
+            this.textBox_Port.Location = new System.Drawing.Point(3, 29);
+            this.textBox_Port.Name = "textBox_Port";
+            this.textBox_Port.Size = new System.Drawing.Size(200, 34);
+            this.textBox_Port.TabIndex = 3;
+            this.textBox_Port.TextChanged += new System.EventHandler(this.TextBox_Port_TextChanged);
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Font = new System.Drawing.Font("宋体", 14F);
+            this.textBox_IP.Location = new System.Drawing.Point(3, 29);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.Size = new System.Drawing.Size(200, 34);
+            this.textBox_IP.TabIndex = 1;
+            this.textBox_IP.TextChanged += new System.EventHandler(this.TextBox_IP_TextChanged);
+            // 
+            // label_IP
+            // 
+            this.label_IP.AutoSize = true;
+            this.label_IP.Font = new System.Drawing.Font("宋体", 14F);
+            this.label_IP.Location = new System.Drawing.Point(3, 0);
+            this.label_IP.Name = "label_IP";
+            this.label_IP.Size = new System.Drawing.Size(28, 26);
+            this.label_IP.TabIndex = 0;
+            this.label_IP.Text = "IP";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(436, 345);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.CheckBox_Override);
+            this.tabPage1.Controls.Add(this.log);
+            this.tabPage1.Controls.Add(this.flowLayoutPanel3);
+            this.tabPage1.Controls.Add(this.flowLayoutPanel2);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Font = new System.Drawing.Font("宋体", 10F);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(428, 315);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "系统代理";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // CheckBox_Override
+            // 
+            this.CheckBox_Override.AutoSize = true;
+            this.CheckBox_Override.Location = new System.Drawing.Point(26, 215);
+            this.CheckBox_Override.Name = "CheckBox_Override";
+            this.CheckBox_Override.Size = new System.Drawing.Size(210, 24);
+            this.CheckBox_Override.TabIndex = 8;
+            this.CheckBox_Override.Text = "本地(intranet)地址不使用代理";
+            this.CheckBox_Override.UseVisualStyleBackColor = true;
+            this.CheckBox_Override.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // log
+            // 
+            this.log.Font = new System.Drawing.Font("宋体", 12F);
+            this.log.Location = new System.Drawing.Point(6, 90);
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.Size = new System.Drawing.Size(415, 110);
+            this.log.TabIndex = 7;
+            this.log.Text = "";
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.label_Port);
+            this.flowLayoutPanel3.Controls.Add(this.textBox_Port);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(215, 6);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(206, 78);
+            this.flowLayoutPanel3.TabIndex = 6;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.label_IP);
+            this.flowLayoutPanel2.Controls.Add(this.textBox_IP);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(6, 6);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(206, 78);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label_versiom);
+            this.tabPage2.Controls.Add(this.linkLabel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(428, 315);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "关于";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label_versiom
+            // 
+            this.label_versiom.AutoSize = true;
+            this.label_versiom.Font = new System.Drawing.Font("宋体", 12F);
+            this.label_versiom.Location = new System.Drawing.Point(22, 36);
+            this.label_versiom.Name = "label_versiom";
+            this.label_versiom.Size = new System.Drawing.Size(61, 22);
+            this.label_versiom.TabIndex = 1;
+            this.label_versiom.Text = "version";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("宋体", 12F);
+            this.linkLabel1.Location = new System.Drawing.Point(22, 70);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(57, 22);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Jump);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(456, 366);
+            this.Controls.Add(this.tabControl1);
+            this.MinimumSize = new System.Drawing.Size(472, 405);
+            this.Name = "Form1";
+            this.Text = "系统代理设置";
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label_IP;
+        private System.Windows.Forms.Label label_Port;
+        private System.Windows.Forms.TextBox textBox_Port;
+        private System.Windows.Forms.TextBox textBox_IP;
+        private System.Windows.Forms.Button button_config;
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.RichTextBox log;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label_versiom;
+        private System.Windows.Forms.CheckBox CheckBox_Override;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace WinProxyTool
 {
     public partial class Form1 : Form
     {
+        readonly string version = "1.1.1 ";
+        int ProxyEnable;
+        string ProxyServer;
+        bool ignore_local;
+
         public Form1()
         {
             InitializeComponent();
+            UpdateValue();
+            label_versiom.Text = version;
         }
+        //内部类
+        private class Proxy_Settings
+        {
+            static readonly RegistryKey proxy = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings", true);
+            //获取代理开关
+            public int Get_ProxyEnable()
+            {
+                int ProxyEnable = (int)proxy.GetValue("ProxyEnable");
+                return ProxyEnable;
+            }
+            public void Set_ProxyEnable(int v) { proxy.SetValue("ProxyEnable", v); }
+            //获取代理IP地址
+            public string Get_ProxyServer()
+            {
+                string ProxyServer = (string)proxy.GetValue("ProxyServer");
+                return ProxyServer;
+            }
+            public void Set_ProxyServer(string s) { proxy.SetValue("ProxyServer", s); }
+
+            //
+            public string Get_ProxyOverride()
+            {
+                string ProxyOverride = (string)proxy.GetValue("ProxyOverride");
+                if (ProxyOverride == null) { return "null"; }
+                else { return ProxyOverride; }
+            }
+            public void Clear_ProxyOverride() { proxy.SetValue("ProxyOverride", ""); }
+        };
+
+        //更新数据
+        private void UpdateValue()
+        {
+            Proxy_Settings Settings = new Proxy_Settings();
+            //代理开关
+            ProxyEnable = Settings.Get_ProxyEnable();
+            //代理地址
+            string mProxyServer = Settings.Get_ProxyServer();
+            if (mProxyServer != null) { ProxyServer = mProxyServer; }
+            //跳过代理
+            string mProxyOverride = Settings.Get_ProxyOverride();
+            ignore_local = mProxyOverride.Contains("<local>");
+            UpdateUI();
+        }
+
+        //更新UI
+        private void UpdateUI()
+        {
+            log.ForeColor = System.Drawing.Color.Black;
+            string on = "代理已开启";
+            string off = "代理已关闭";
+            if (ProxyEnable == 1) { button1.Text = on; }
+            else { button1.Text = off; }
+            if (log.Text.Length > 20) { log.Text = ""; }
+
+            try
+            {
+                int index = ProxyServer.IndexOf("://");
+                if (index == ProxyServer.IndexOf(":")) { ProxyServer = (string)ProxyServer.Substring(index + 3); }
+
+                if (ProxyServer.IndexOf("://") == -1)
+                {
+                    textBox_IP.Text = (string)ProxyServer.Split(':')[0];
+                    textBox_Port.Text = (string)ProxyServer.Split(':')[1];
+                    log.Text = string.Format("代理状态: {0}\n代理地址: {1}\n", button1.Text.Substring(2), ProxyServer);
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e); }
+
+            if (ignore_local) { CheckBox_Override.Checked = true; CheckBox_Override.Enabled = true; } else { CheckBox_Override.Checked = false; CheckBox_Override.Enabled = false; }
+        }
+
+        //设置代理开关
+        private void Set_ProxyEnable(int v)
+        {
+            Proxy_Settings Settings = new Proxy_Settings();
+            Settings.Set_ProxyEnable(v);
+        }
+
+        //设置代理地址
+        private void Save_ProxyServer()
+        {
+            Proxy_Settings Settings = new Proxy_Settings();
+            string _ip = textBox_IP.Text.Trim();
+            string _port = textBox_Port.Text.Trim();
+
+            if (_ip != "" && _ip.IndexOf(" ") == -1)
+            {
+                if (_port != "" && _port.IndexOf(" ") == -1 && int.TryParse(_port, out _))
+                {
+                    Settings.Set_ProxyServer(_ip + ":" + _port);
+                    UpdateValue();
+                }
+                else { log.ForeColor = System.Drawing.Color.Red; log.Text = "端口错误\n" + _port; }
+            }
+            else { log.ForeColor = System.Drawing.Color.Red; log.Text = "IP错误\n" + _ip; }
+            log.ForeColor = System.Drawing.Color.Black;
+        }
+
+        //设置跳过本地地址
+        private void Save_ProxyOverride()
+        {
+            Proxy_Settings Settings = new Proxy_Settings();
+            Settings.Clear_ProxyOverride();
+            UpdateValue();
+        }
+
+        //刷新按钮
+        private void Button_get_Proxy_Click(object sender, EventArgs e) { UpdateValue(); }
+
+        //保存代理按钮
+        private void Button_Save_Proxy_Click(object sender, EventArgs e) { Save_ProxyServer(); Save_ProxyOverride(); }
+
+        //代理开关按钮
+        private void Button_Enable_Proxy_Click(object sender, EventArgs e)
+        {
+            if (ProxyEnable == 1) { Set_ProxyEnable(0); }
+            else { Set_ProxyEnable(1); }
+            UpdateValue();
+        }
+
+        private void Jump(object sender, LinkLabelLinkClickedEventArgs e) { Process.Start("https://github.com/yinleren6/WinRegTool/releases"); }
+
+        private void TextBox_IP_TextChanged(object sender, EventArgs e) { log.Text = "未保存"; }
+
+        private void TextBox_Port_TextChanged(object sender, EventArgs e) { log.Text = "未保存"; }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e) { if (!CheckBox_Override.Checked) { Save_ProxyOverride(); } }
     }
 }
