95558328242a1d771a7cf25204e90b7f9670f960
misaka <zelda.me@outlook.com>
2023-07-10 17:02:08 +0800

    新增 -现在可以识别IP和端口有效性了

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # WinProxyTool
 
-**此项目将迁移到 [WinProxyTool_WPF](https://github.com/yinleren6/WinProxyTool_WPF)**
+**另一个更好看的版本 [WinProxyTool_WPF](https://github.com/yinleren6/WinProxyTool_WPF)**
 
 ___
 
-一个快速设置Windows代理的小工具
+一个快速设置Windows代理的小工具 v1.2
 A gadget for quickly setting up Windows agents
--- main.cs
+++ main.cs
@@ -1,57 +1,29 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Diagnostics;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using WinProxyTool.Utils;
 
 namespace WinProxyTool
 {
     public partial class MainWindow : Form
     {
-        readonly string version = "1.1.2 ";
         int ProxyEnable;
         string ProxyServer;
         bool ignore_local;
+        RegistryTool Settings = new RegistryTool();
 
         public MainWindow()
         {
             InitializeComponent();
-            this.CheckBox_Override.Text = "本地(intranet)地址不使用代理";
-            UpdateValue(); UpdateUI();
-            label_versiom.Text = version;
+            UpdateValue();
+            UpdateUI();
         }
-        //内部类
-        private class Proxy_Settings
-        {
-            static readonly RegistryKey proxy = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings", true);
-            //获取代理开关
-            public int Get_ProxyEnable()
-            {
-                int ProxyEnable = (int)proxy.GetValue("ProxyEnable");
-                return ProxyEnable;
-            }
-            public void Set_ProxyEnable(int v) { proxy.SetValue("ProxyEnable", v); }
-            //获取代理IP地址
-            public string Get_ProxyServer()
-            {
-                string ProxyServer = (string)proxy.GetValue("ProxyServer");
-                return ProxyServer;
-            }
-            public void Set_ProxyServer(string s) { proxy.SetValue("ProxyServer", s); }
-
-            //
-            public string Get_ProxyOverride()
-            {
-                string ProxyOverride = (string)proxy.GetValue("ProxyOverride");
-                if (ProxyOverride == null) { return "null"; }
-                else { return ProxyOverride; }
-            }
-            public void Clear_ProxyOverride() { proxy.SetValue("ProxyOverride", ""); }
-        };
 
         //更新数据
         private void UpdateValue()
         {
-            Proxy_Settings Settings = new Proxy_Settings();
             //代理开关
             ProxyEnable = Settings.Get_ProxyEnable();
             //代理地址
@@ -86,64 +58,78 @@ namespace WinProxyTool
             }
             catch (Exception e) { Console.WriteLine(e); }
 
-            if (ignore_local) { CheckBox_Override.Checked = true; CheckBox_Override.Enabled = true; } else { CheckBox_Override.Checked = false; CheckBox_Override.Enabled = false; }
+
         }
 
         //设置代理开关
         private void Set_ProxyEnable(int v)
         {
-            Proxy_Settings Settings = new Proxy_Settings();
             Settings.Set_ProxyEnable(v);
         }
 
         //设置代理地址
-        private void Save_ProxyServer()
+        private int Save_ProxyServer()
         {
-            Proxy_Settings Settings = new Proxy_Settings();
             string _ip = textBox_IP.Text.Trim();
             string _port = textBox_Port.Text.Trim();
+            Regex validipregex = new Regex(@"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+            bool ip_valid = (_ip != "" && validipregex.IsMatch(_ip)) ? true : false;
 
-            if (_ip != "" && _ip.IndexOf(" ") == -1)
+            if (_ip != ""&& ip_valid){}
+            else
             {
-                if (_port != "" && _port.IndexOf(" ") == -1 && int.TryParse(_port, out _))
+                log.ForeColor = System.Drawing.Color.Red; log.Text = "IP错误\n" + _ip;
+                return -1;
+            }
+
+            if (_port != "" &&   int.TryParse(_port, out _))
+            {
+                int m_port = int.Parse(_port);
+                if (m_port > 0 && m_port < 65536)
                 {
                     Settings.Set_ProxyServer(_ip + ":" + _port);
                     UpdateValue();
+                    return 0;
                 }
-                else { log.ForeColor = System.Drawing.Color.Red; log.Text = "端口错误\n" + _port; }
+                else
+                {
+                    log.ForeColor = System.Drawing.Color.Red; log.Text = "端口错误\n" + _port;
+                    return -1;
+                }
             }
-            else { log.ForeColor = System.Drawing.Color.Red; log.Text = "IP错误\n" + _ip; }
-            log.ForeColor = System.Drawing.Color.Black;
+            else
+            {
+                log.ForeColor = System.Drawing.Color.Red; log.Text = "端口错误\n" + _port;
+                return -1;
+            }
         }
 
-        //设置跳过本地地址
-        private void Save_ProxyOverride()
-        {
-            Proxy_Settings Settings = new Proxy_Settings();
-            Settings.Clear_ProxyOverride();
-            UpdateValue();
-        }
+
 
         //刷新按钮
         private void Button_get_Proxy_Click(object sender, EventArgs e) { UpdateValue(); UpdateUI(); }
 
         //保存代理按钮
-        private void Button_Save_Proxy_Click(object sender, EventArgs e) { Save_ProxyServer(); Save_ProxyOverride(); UpdateValue(); UpdateUI(); }
+        private void Button_Save_Proxy_Click(object sender, EventArgs e)
+        {
+            if (Save_ProxyServer() == 1) { UpdateValue(); UpdateUI(); }
+        }
 
         //代理开关按钮
         private void Button_Enable_Proxy_Click(object sender, EventArgs e)
         {
             if (ProxyEnable == 1) { Set_ProxyEnable(0); }
             else { Set_ProxyEnable(1); }
-            UpdateValue(); UpdateUI();
+            UpdateValue();
+            UpdateUI();
         }
 
-        private void Jump(object sender, LinkLabelLinkClickedEventArgs e) { Process.Start("https://github.com/yinleren6/WinRegTool/releases"); }
+        private void Github_Release(object sender, LinkLabelLinkClickedEventArgs e) { Process.Start("https://github.com/yinleren6/WinRegTool/"); }
 
         private void TextBox_IP_TextChanged(object sender, EventArgs e) { log.Text = "未保存"; }
 
         private void TextBox_Port_TextChanged(object sender, EventArgs e) { log.Text = "未保存"; }
 
-        private void CheckBox1_CheckedChanged(object sender, EventArgs e) { if (!CheckBox_Override.Checked) { Save_ProxyOverride(); } }
+
     }
 }
